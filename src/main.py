2a2016415568bb18d304139450ea79031dd1bda7
Hannes Mann <hannesmann2000@gmail.com>
2023-04-19 16:55:49 +0200

    Add file for managing serial number

--- src/main.py
+++ src/main.py
@@ -2,12 +2,16 @@ import platform
 import sys
 import time
 
+import serial
+current_serial = serial.get_serial_number()
+
 # TODO: Check if we are running on Raspberry Pi
 system = f"{platform.uname().system} {platform.uname().release}"
 
 print("Green Garden IoT Controller started")
 print(f"Python: {sys.version_info.major}.{sys.version_info.minor}")
 print(f"System: {system}")
+print(f"Serial number: '{current_serial}'")
 print("")
 
 try:
