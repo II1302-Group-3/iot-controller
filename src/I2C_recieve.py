1242c6724a8a85a5e0557dd1caddbe9b192129da
Emil Scott-Robbert <90572005+FenixAuriga@users.noreply.github.com>
2023-05-04 18:49:29 +0200

    Update I2C_recieve.py
    Fixed indentation

--- src/I2C_recieve.py
+++ src/I2C_recieve.py
@@ -17,23 +17,23 @@ bus = smbus.SMBus(1)
 
 address = 0x8
 
-	data = bytearray()
-	for i in range(0,6):
-		data.append(bus.read_byte(address));
+data = bytearray()
+for i in range(0,6):
+	data.append(bus.read_byte(address));
 		
-	temp_b1 = int(data.pop(0))
-	temp_b0 = int(data.pop(0))
-	humidity_b1	= int(data.pop(0))
-	humidity_b0 = int(data.pop(0))
-	moisture_b1	= int(data.pop(0))
-	moisture_b0 = int(data.pop(0))
+temp_b1 = int(data.pop(0))
+temp_b0 = int(data.pop(0))
+humidity_b1	= int(data.pop(0))
+humidity_b0 = int(data.pop(0))
+moisture_b1	= int(data.pop(0))
+moisture_b0 = int(data.pop(0))
 	
-	temp = temp_b0 + temp_b1*256
-	humidity = humidity_b0 + humidity_b1 *256
-	moisture = moisture_b0 + moisture_b1 *256
+temp = temp_b0 + temp_b1*256
+humidity = humidity_b0 + humidity_b1 *256
+moisture = moisture_b0 + moisture_b1 *256
 
 	
-	time.sleep(2);
+#time.sleep(2);
 	
 		
 		
