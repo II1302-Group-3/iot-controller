8d3d78d79386a03d0f55ad7b9fb2d2a77c1bc94c
Hannes Mann <hannesmann2000@gmail.com>
2023-05-09 01:35:51 +0200

    Add a timeout for all network requests

--- src/main.py
+++ src/main.py
@@ -3,6 +3,22 @@ import sys
 from termcolor import colored
 from time import sleep
 
+import requests
+from requests.adapters import TimeoutSauce
+
+# https://stackoverflow.com/questions/17782142/why-doesnt-requests-get-return-what-is-the-default-timeout-that-requests-get
+# Set a timeout for all requests, this needs to be done at the start
+class GlobalTimeout(TimeoutSauce):
+	def __init__(self, *args, **kwargs):
+		if kwargs["connect"] is None:
+			kwargs["connect"] = 5
+		if kwargs["read"] is None:
+			kwargs["read"] = 5
+
+		super(GlobalTimeout, self).__init__(*args, **kwargs)
+
+requests.adapters.TimeoutSauce = GlobalTimeout
+
 import authentication
 login = authentication.get_serial_and_key()
 
