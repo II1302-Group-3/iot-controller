8585669e2ddecd153de8a26188e99e9a5c3d4e0f
Hannes Mann <hannesmann2000@gmail.com>
2023-05-11 15:25:15 +0200

    Fix syntax error

--- src/moisture.py
+++ src/moisture.py
@@ -7,11 +7,14 @@ import plant_detector
 previous_target_moisture = 0
 
 def update(database):
+	global previous_target_moisture
+
 	target_moisture = database.target_moisture
 	if not plant_detector.detected_plant:
 		target_moisture = 0
 
-	if database.target_moisture != previous_target_moisture:
+	if target_moisture != previous_target_moisture:
 		sleep(1)
-		print(f"Sending moisture to Arduino: {m}")
+		print(f"Sending moisture to Arduino: {target_moisture}")
 		i2c_arduino_init.bus.write_word_data(i2c_arduino_init.address,0x00,target_moisture)
+		previous_target_moisture = target_moisture
