9df5a7c6f1f6772e4db86969429d9ee27bf55ad8
Hannes Mann <hannesmann2000@gmail.com>
2023-05-11 15:26:03 +0200

    ewfpoejwqfpoewqojfpewq

--- src/moisture.py
+++ src/moisture.py
@@ -11,7 +11,7 @@ def update(database):
 	if not plant_detector.detected_plant:
 		target_moisture = 0
 
-	if database.target_moisture != previous_target_moisture:
+	if target_moisture != previous_target_moisture:
 		sleep(1)
 		print(f"Sending moisture to Arduino: {target_moisture}")
 		i2c_arduino_init.bus.write_word_data(i2c_arduino_init.address,0x00,target_moisture)
